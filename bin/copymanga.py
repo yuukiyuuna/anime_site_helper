75979e5e091c30bc3e1ecdb9b4c6eaa23c10314d
hancl <qq984512625@gmail.com>
2022-08-27 17:26:11 +0800

    修复copy漫画下载逻辑并增加选择下载功能

--- bin/copymanga.py
+++ bin/copymanga.py
@@ -1,6 +1,8 @@
 # -*- coding=utf-8 -*-
 
 import os, time
+import random
+
 from loguru import logger
 import requests
 # 关闭证书验证（copy漫画下载图片时需要验证）
@@ -15,11 +17,13 @@ class copymanga():
             'source': 'copyApp',
             'version': '1.3.1',
             'region': '1',
-            'webp': '0'
+            'webp': '0',
+            'platform': '3'
         }
-        self.api_url = r'https://api.copymanga.org/'
+        self.api_url = r'https://api.copymanga.site/'
+        logger.info('接口地址设定为：%s' % self.api_url)
 
-    def download(self, url, output_dir=None):
+    def download(self, url, output_dir=None, start=None, end=None):
         if url.endswith('/'):
             manga_name = url.split('/')[-2]
         else:
@@ -27,7 +31,35 @@ class copymanga():
         logger.info('识别漫画名称：%s' % manga_name)
 
         # 获取章节列表
-        mangas_zhangjie = self.get_chapters(manga_name=manga_name)
+        mangas_zhangjie_info = self.get_chapters(manga_name=manga_name)
+        logger.info('获取章节成功，共获取 %d 个章节' %mangas_zhangjie_info['manga_len'])
+        logger.debug(mangas_zhangjie_info)
+
+        # 匹配需要下载的部分
+        if str(start).isdigit():
+            start = int(start) - 1
+        elif not str(start).isdigit():
+            if start in mangas_zhangjie_info['manga_title_list']:
+                start = mangas_zhangjie_info['manga_title_list'].index(start)
+            else:
+                start = 0
+        else:
+            start = 0
+
+        if str(end).isdigit():
+            end = int(end)
+        elif not str(end).isdigit():
+            if end in mangas_zhangjie_info['manga_title_list']:
+                end = mangas_zhangjie_info['manga_title_list'].index(end) + 1
+            else:
+                end = mangas_zhangjie_info['manga_len']
+        else:
+            end = mangas_zhangjie_info['manga_len']
+
+        mangas_zhangjie = []
+        for num in range(start, end):
+            mangas_zhangjie.append(mangas_zhangjie_info['manga_info_list'][num])
+        logger.info('成功筛选出下载范围')
         logger.debug(mangas_zhangjie)
 
         # 创建漫画文件夹
@@ -35,7 +67,6 @@ class copymanga():
             manga_dir = os.path.join(output_dir, manga_name)
             if not os.path.exists(manga_dir):
                 os.mkdir(manga_dir)
-
         else:                           # 若未指定输出文件夹
             manga_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], manga_name)
             if not os.path.exists(manga_dir):
@@ -61,7 +92,7 @@ class copymanga():
                 logger.info('开始下载 %s 中的第 %d 图片' % (manga[0], page[1]))
                 self.download_page(url=page[0], filepath=manga_page_dir)
 
-                time.sleep(1)
+                time.sleep(random.randint(3, 5))
 
 
 
@@ -69,25 +100,47 @@ class copymanga():
 
     # 获取章节列表
     def get_chapters(self, manga_name):
-        path = r'api/v3/comic/%s/group/default/chapters?limit=500&offset=0&platform=3' % manga_name
-        url = self.api_url + path
 
-        req = requests.get(url, headers=self.headers)
-        if req.status_code == 200:
-            if req.json()['code'] == 200:
-                logger.info('章节列表获取成功，共计 %d 话' % req.json()['results']['total'])
+        limit = 20
+        offset = 0
+        mangas_info = []
+        manga_title_name = []
+        while True:
+            path = r'api/v3/comic/%s/group/default/chapters?limit=%d&offset=%d&format=json' \
+                   %(manga_name, limit, offset)
+            url = self.api_url + path
+
+            req = requests.get(url, headers=self.headers)
+            # req = requests.get(url, headers=self.headers)
+            if req.status_code == 200:
+                if req.json()['code'] == 200:
+                    logger.debug('成功访问 %s' %req.request.url)
+                    logger.debug(req.json())
+                    logger.info('本次请求共获取 %d 个章节' %len(req.json()['results']['list']))
+
+                    for manga in req.json()['results']['list']:
+                        mangas_info.append([manga['name'], manga['uuid']])
+                        manga_title_name.append(manga['name'])
+
+                    if offset + limit >= req.json()['results']['total']:
+                        break
+
+                    offset += limit
+
+
+
+
+
+                else:
+                    logger.error('章节列表获取失败，返回码 %d' % req.json()['code'])
+                    logger.error(req.teaxt)
+                    continue
             else:
-                logger.error('章节列表获取失败')
+                logger.error('章节列表获取失败，返回码 %d' % req.status_code)
                 logger.error(req.text)
-        else:
-            logger.error('章节列表获取失败')
-            logger.error(req.text)
+                continue
 
-        mangas = []
-        for manga in req.json()['results']['list']:
-            mangas.append([manga['name'], manga['uuid']])
-
-        return mangas
+        return {'manga_len': len(mangas_info), 'manga_title_list': manga_title_name, 'manga_info_list': mangas_info}
 
 
     # 获取章节内列表
@@ -146,4 +199,3 @@ class copymanga():
             logger.warning('下载图片失败，一秒后重试')
             logger.error(e)
             self.download_page(url=url, filepath=filepath)
-
