3f40cf2cff6a6ab49c7ef671cc89d8abb68adc6f
hancl <qq984512625@gmail.com>
2022-08-28 21:35:24 +0800

    增加输出目录

--- file_zip.py
+++ file_zip.py
@@ -2,19 +2,20 @@
 import os
 import zipfile
 
-def zip_file(src_dir, save_name='default'):
+def zip_file(src_dir, save_name='default', save_dir='./'):
     '''
     压缩文件夹下所有文件及文件夹
     默认压缩文件名：文件夹名
     默认压缩文件路径：文件夹上层目录
     '''
     if save_name == 'default':
-        zip_name = src_dir + '.zip'
+        zip_name = os.path.join(save_dir, src_dir + '.zip')
     else:
         if save_name is None or save_name == '':
-            zip_name = src_dir + '.zip'
+            zip_name = os.path.join(save_dir, src_dir + '.zip')
         else:
-            zip_name = save_name + '.zip'
+            zip_name = os.path.join(save_dir, save_name + '.zip')
+    print(zip_name)
 
     z = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
     for dirpath, dirnames, filenames in os.walk(src_dir):
@@ -27,12 +28,12 @@ def zip_file(src_dir, save_name='default'):
 
 
 
-dir = r'F:\linshi\kuangduzhiyuanwang'
+dir = r'F:\linshi\test'
 
 files = os.listdir(dir)
 
 for file in files:
-    zip_file(os.path.join(dir, file), save_name=file)
-    os.rename(file + r'.zip', file + r'.cbz')
+    zip_file(os.path.join(dir, file), save_name=file, save_dir=dir)
+    # os.rename(file + r'.zip', file + r'.cbz')
 
 
